86853fe0c779eaa87e5e6be942e7735cd16019e0
Caroline Tan <carolinectan3@gmail.com>
2021-09-04 00:29:59 -0400

    Refactor merchants index action

--- app/controllers/api/v1/merchants_controller.rb
+++ app/controllers/api/v1/merchants_controller.rb
@@ -1,9 +1,22 @@
 class Api::V1::MerchantsController < ApplicationController
   def index
-    merchants = Merchant.paginate(page: params[:page], per_page: 20)
-    x = render json: MerchantSerializer.new(merchants)
-    # require "pry"; binding.pry
+    if params[:page] && params[:per_page]
+      merchants = Merchant.paginate(page: params[:page], per_page: params[:per_page])
+    elsif params[:page]
+      merchants = Merchant.paginate(page: params[:page], per_page: 20)
+    # elsif params[:page] == "1"
+    #   merchants = Merchant.paginate(page: params[:page], per_page: 50)
+    elsif params[:per_page]
+      merchants = Merchant.paginate(page: 1, per_page: params[:per_page])
+    # elsif params[:page].to_i <= 0
+    #   require "pry"; binding.pry
+      # merchants = Merchant.paginate(page: 1, per_page: 20)
+    else
+      merchants = Merchant.paginate(page: 1, per_page: 20)
+    end
+    render json: MerchantSerializer.new(merchants)
   end
+
   # def show
   #
   # end
