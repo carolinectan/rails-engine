c8937a890967ad79d6d7de7bb82376125c3307b3
Caroline Tan <carolinectan3@gmail.com>
2021-09-04 00:56:38 -0400

    Finish merchants index action

--- app/controllers/api/v1/merchants_controller.rb
+++ app/controllers/api/v1/merchants_controller.rb
@@ -2,15 +2,10 @@ class Api::V1::MerchantsController < ApplicationController
   def index
     if params[:page] && params[:per_page]
       merchants = Merchant.paginate(page: params[:page], per_page: params[:per_page])
-    elsif params[:page]
+    elsif params[:page].to_i > 0
       merchants = Merchant.paginate(page: params[:page], per_page: 20)
-    # elsif params[:page] == "1"
-    #   merchants = Merchant.paginate(page: params[:page], per_page: 50)
     elsif params[:per_page]
       merchants = Merchant.paginate(page: 1, per_page: params[:per_page])
-    # elsif params[:page].to_i <= 0
-    #   require "pry"; binding.pry
-      # merchants = Merchant.paginate(page: 1, per_page: 20)
     else
       merchants = Merchant.paginate(page: 1, per_page: 20)
     end
