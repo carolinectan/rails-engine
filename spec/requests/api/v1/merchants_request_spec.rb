8d80e036a52066082750b3a2e95cd00d88934fc6
Caroline Tan <carolinectan3@gmail.com>
2021-09-05 16:49:09 -0400

    Add get one merchant test

--- spec/requests/api/v1/merchants_request_spec.rb
+++ spec/requests/api/v1/merchants_request_spec.rb
@@ -22,15 +22,19 @@ describe 'Merchants API' do
     end
   end
 
-  # it 'can get one merchant by its id' do
-  #   id = create(merchant).id
-  #
-  #   get "/api/v1/merchants/#{id}"
-  #
-  #   merchant = JSON.parse(response.body, symbolize_names: true)
-  #
-  #   expect(response).to be_successful
-  #
-  #   expect(merchan)
-  # end
+  it 'can get one merchant by its id' do
+    id = create(:merchant).id
+
+    get "/api/v1/merchants/#{id}"
+
+    merchant = JSON.parse(response.body, symbolize_names: true)
+
+    expect(response).to be_successful
+
+    expect(merchant).to have_key(:id)
+    expect(merchant[:id]).to eq(id)
+
+    expect(merchant).to have_key(:name)
+    expect(merchant[:name]).to be_a(String)
+  end
 end
