dfc4d1dacaee268853789efba1ed2f5d1d7e6e57
Caroline Tan <carolinectan3@gmail.com>
2021-09-05 22:46:00 -0400

    Refactor

--- app/controllers/api/v1/merchants_controller.rb
+++ app/controllers/api/v1/merchants_controller.rb
@@ -1,10 +1,11 @@
 class Api::V1::MerchantsController < ApplicationController
   def index
     if params[:page].to_i <= 1
-      render json: MerchantSerializer.new(Merchant.paginate(page: 1, per_page: params[:per_page]))
+      merchant = Merchant.paginate(page: 1, per_page: params[:per_page])
     else
-      render json: MerchantSerializer.new(Merchant.paginate(page: params[:page], per_page: params[:per_page]))
+      merchant = Merchant.paginate(page: params[:page], per_page: params[:per_page])
     end
+    render json: MerchantSerializer.new(merchant)
 
     # if params[:page] && params[:per_page]
     #   merchants = Merchant.paginate(page: params[:page], per_page: params[:per_page])
@@ -20,7 +21,8 @@ class Api::V1::MerchantsController < ApplicationController
 
   def show
     # render json: Merchant.find(params[:id])
-    render json: MerchantSerializer.new(Merchant.find(params[:id]))
+    merchant = Merchant.find(params[:id])
+    render json: MerchantSerializer.new(merchant)
   end
 
   # def new
