fedd9f2168dc25f6c5065ea1cddf5f6118721a6d
Caroline Tan <carolinectan3@gmail.com>
2021-09-06 14:43:49 -0400

    Add tests

--- spec/requests/api/v1/merchants_request_spec.rb
+++ spec/requests/api/v1/merchants_request_spec.rb
@@ -1,24 +1,48 @@
 require 'rails_helper'
 
 describe 'Merchants API' do
-  it 'sends a list of merchants' do
-    create_list(:merchant, 25)
+  describe 'get all merchants' do
+    it 'sends a list of 20 merchants when fetching page 1' do
+      create_list(:merchant, 25)
 
-    get '/api/v1/merchants', params: { page: 1 }
-    # get '/api/v1/merchants?page=1
+      get '/api/v1/merchants', params: { page: 1 }
 
-    expect(response).to be_successful
+      expect(response).to be_successful
 
-    merchants = JSON.parse(response.body, symbolize_names: true)
+      merchants = JSON.parse(response.body, symbolize_names: true)
 
-    expect(merchants[:data].count).to eq(20)
+      expect(merchants[:data].count).to eq(20)
 
-    merchants[:data].each do |merchant|
-      expect(merchant).to have_key(:id)
-      expect(merchant[:id]).to be_a(String)
+      merchants[:data].each do |merchant|
+        expect(merchant).to have_key(:id)
+        expect(merchant[:id]).to be_a(String)
 
-      expect(merchant[:attributes]).to have_key(:name)
-      expect(merchant[:attributes][:name]).to be_a(String)
+        expect(merchant[:attributes]).to have_key(:name)
+        expect(merchant[:attributes][:name]).to be_a(String)
+      end
+    end
+
+    it 'fetches page 1 of 20 people if page is 0 or lower' do
+      create_list(:merchant, 25)
+
+      get '/api/v1/merchants', params: { page: 0 }
+
+      expect(response).to be_successful
+
+      merchants = JSON.parse(response.body, symbolize_names: true)
+
+      expect(merchants[:data].count).to eq(20)
+
+      merchants[:data].each do |merchant|
+        expect(merchant).to have_key(:id)
+        expect(merchant[:id]).to be_a(String)
+
+        expect(merchant[:attributes]).to have_key(:name)
+        expect(merchant[:attributes][:name]).to be_a(String)
+      end
+
+      expect(merchants[:data].first[:id].to_i).to eq(Merchant.first.id)
+      expect(merchants[:data].last[:id].to_i).to_not eq(Merchant.last.id)
     end
   end
 
