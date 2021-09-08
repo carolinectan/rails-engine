07d03903d266fff700b7d98ae0385ebf65cd4114
Caroline Tan <carolinectan3@gmail.com>
2021-09-08 17:34:30 -0400

    Remove dep destroy

--- app/models/item.rb
+++ app/models/item.rb
@@ -6,7 +6,7 @@ class Item < ApplicationRecord
   belongs_to :merchant
 
   has_many :invoice_items, dependent: :destroy
-  has_many :invoices, through: :invoice_items, dependent: :destroy
+  has_many :invoices, through: :invoice_items#, dependent: :destroy
 
   self.per_page = 20
 end
