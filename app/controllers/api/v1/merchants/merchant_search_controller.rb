af57036a583de1c75dd0d38c4c29126fb2fe9760
Caroline Tan <carolinectan3@gmail.com>
2021-09-09 12:33:00 -0400

    Remove comments

--- app/controllers/api/v1/merchants/merchant_search_controller.rb
+++ app/controllers/api/v1/merchants/merchant_search_controller.rb
@@ -4,18 +4,7 @@ class Api::V1::Merchants::MerchantSearchController < ApplicationController
       merchant = Merchant.where('name ILIKE ?', "%#{params[:name]}%").first
       render json: MerchantSerializer.new(merchant)
     else
-      # render json: [{}]
-      # render json: MerchantSerializer.new(data: null)
-      # render json: MerchantSerializer.new({data: []})
-      # render json: {} # => sad path, no fragment matched | AssertionError: expected {} to have property 'data'
-      render json: {data: {}} # =>sad path, no fragment matched | AssertionError: expected { Object (status, error, ...) } to have property 'data'
-      # render(json: null) # not this
+      render json: {data: {}}
     end
   end
-
-  private
-  #
-  # def no_fragment_matched
-  #   render json: {data: null}, status: 200
-  # end
 end
