1803f6647511d63b0602e5c785f0d00e32cf103f
Caroline Tan <carolinectan3@gmail.com>
2021-09-08 20:47:11 -0400

    Add tests for item's merchant

--- spec/requests/api/v1/item_merchant_request_spec.rb
+++ spec/requests/api/v1/item_merchant_request_spec.rb
@@ -10,24 +10,24 @@ describe "item's merchant API" do
 
       expect(response).to be_successful
 
-      # is this the right way to test this?
-      expect(merchant_id).to eq(Item.find(item_id).merchant_id)
+      merchant = JSON.parse(response.body, symbolize_names: true)
 
-      items = JSON.parse(response.body, symbolize_names: true)
-      #
-      # expect(items[:data].count).to eq(20)
-      # expect(items[:data]).to be_an(Array)
-      # expect(items[:data].first[:id].to_i).to eq(Item.first.id)
-      #
-      # items[:data].each do |item|
-      #   expect(item.length).to eq(3)
-      #   expect(item).to have_key(:id)
-      #   expect(item[:id]).to be_a(String)
-      #
-      #   expect(item).to have_key(:type)
-      #   expect(item[:type]).to be_a(String)
+      expect(merchant[:data].length).to eq(3)
+      expect(merchant[:data]).to be_a(Hash)
 
+      expect(merchant[:data]).to have_key(:id)
+      expect(merchant[:data][:id]).to be_a(String)
+      expect(merchant[:data][:id]).to eq(merchant_id.to_s)
 
+      expect(merchant[:data]).to have_key(:type)
+      expect(merchant[:data][:type]).to be_a(String)
+
+      expect(merchant[:data]).to have_key(:attributes)
+      expect(merchant[:data][:attributes].length).to eq(1)
+      expect(merchant[:data][:attributes]).to be_a(Hash)
+
+      expect(merchant[:data][:attributes]).to have_key(:name)
+      expect(merchant[:data][:attributes][:name]).to be_a(String)
     end
 
     xit 'returns a 404 if the item is not found' do
