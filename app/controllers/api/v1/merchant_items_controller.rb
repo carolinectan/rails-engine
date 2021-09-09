94d978d8a7651e20476f282e1f43ab4204fd07a6
Caroline Tan <carolinectan3@gmail.com>
2021-09-08 21:17:31 -0400

    Refactor

--- app/controllers/api/v1/merchant_items_controller.rb
+++ app/controllers/api/v1/merchant_items_controller.rb
@@ -1,7 +1,10 @@
 class Api::V1::MerchantItemsController < ApplicationController
   def index
-    merchant = Merchant.find(params[:merchant_id])
-    items = Item.where(merchant_id: params[:merchant_id])
-    render json: ItemSerializer.new(items)
+    if params[:merchant_id] && Merchant.find(params[:merchant_id])
+      items = Item.where(merchant_id: params[:merchant_id])
+      render json: ItemSerializer.new(items, status: 200)
+    else
+      not_found_404
+    end
   end
 end
